5b8d534c43ea993768e7a5f7a3785f8b6f53c570
Steelbook <Steelbook@Steelbooks-MacBook-Pro.local>
2014-08-26 12:49:51 +0200

    added a test class

---
+++ SDTest.h
@@ -0,0 +1,13 @@
+//
+//  SDTest.h
+//  Git Test
+//
+//  Created by Steelbook on 26.08.14.
+//  Copyright (c) 2014 Spartanic Designs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDTest : NSObject
+
+@end
---
+++ SDTest.m
@@ -0,0 +1,13 @@
+//
+//  SDTest.m
+//  Git Test
+//
+//  Created by Steelbook on 26.08.14.
+//  Copyright (c) 2014 Spartanic Designs. All rights reserved.
+//
+
+#import "SDTest.h"
+
+@implementation SDTest
+
+@end
